8299f262b97b1c2f0f1228e96941a0411e4da9fd
ashley-monaghan <113056162+ashley-monaghan@users.noreply.github.com>
2022-10-16 18:03:09 -0400

    Added more code

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Copyright (c) 2022 Ashley Monaghan all rights reserved
+//
+// Created by: Ashley Monaghan
+// Created on: Oct 2022
+// This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates area and perimeter of triangle.
+ */
+function calculate () {
+  // input
+  const length = parseInt(document.getElementById('length-of-rectangle').value)
+  const width = parseInt(document.getElementById('width-of-rectangle').value)
+
+  // process
+  const area = length * width / 2
+  const perimeter = 3 * (length)
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+  document.getElementById('perimeter').innerHTML = 'Perimeter is: ' + perimeter + ' cm'
+}
