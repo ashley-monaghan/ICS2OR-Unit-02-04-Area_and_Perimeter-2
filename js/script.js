ee79b4b72870efd0cd47ebfc024966a7a77e60d4
ashley-monaghan <ashley-monaghan@users.noreply.github.com>
2022-10-16 22:04:57 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -4,20 +4,21 @@
 // Created on: Oct 2022
 // This file contains the JS functions for index.html
 
-'use strict'
+"use strict"
 /**
  * This function calculates area and perimeter of triangle.
  */
-function calculate () {
+function calculate() {
   // input
-  const length = parseInt(document.getElementById('length-of-rectangle').value)
-  const width = parseInt(document.getElementById('width-of-rectangle').value)
+  const length = parseInt(document.getElementById("length-of-rectangle").value)
+  const width = parseInt(document.getElementById("width-of-rectangle").value)
 
   // process
-  const area = length * width / 2
-  const perimeter = 3 * (length)
+  const area = (length * width) / 2
+  const perimeter = 3 * length
 
   // output
-  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
-  document.getElementById('perimeter').innerHTML = 'Perimeter is: ' + perimeter + ' cm'
+  document.getElementById("area").innerHTML = "Area is: " + area + " cm²"
+  document.getElementById("perimeter").innerHTML =
+    "Perimeter is: " + perimeter + " cm"
 }
